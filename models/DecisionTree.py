746449d695beaf43ab5826756e775c5eb0852819
AxelitoEnGithubsito <axel.monge.ramirez@est.una.ac.cr>
2024-05-06 19:08:32 -0600

    comentarios + cambios de nombre

--- models/DecisionTree.py
+++ models/DecisionTree.py
@@ -3,7 +3,14 @@ import pandas as pd
 
 class Nodo:
     def __init__(self, es_hoja=False, regla=None, etiqueta=None, impureza=0, muestras=0):
-        # Inicializa un nodo del árbol de decisión.
+        """
+        Inicializa un nodo del árbol de decisión, cada nodo tiene su regla de división además
+        de un atributo que indica si es un nodo final (hoja). Nuestro algoritmo está diseñado
+        para que las ramas del nodo siempre sean binarias, por lo que si hay un atributo multiclase
+        los hijos del nodo corresponderían a si dicho atributo presenta la subclase o no. 
+        Etiqueta es el nombre del target, se utilizaa solo cuando el nodo es hoja al momento de 
+        representarlo gráficamente.
+        """
         self.es_hoja = es_hoja
         self.regla = regla
         self.etiqueta = etiqueta
@@ -13,11 +20,32 @@ class Nodo:
         self.muestras = muestras
 
     def __str__(self):
-        # Representación en string de un nodo.
+        """
+        Describe a un nodo dependiendo si es hoja o no. En caso de no ser hoja, se muestra la regla
+        y en caso de ser hoja, se muestra la etiqueta (nombre del target).
+        """
         return f"Hoja: {self.etiqueta}" if self.es_hoja else f"Regla: {self.regla}"
 
-class ArbolDecision:
+class DecisionTree:
     def __init__(self, caracteristicas, etiquetas, criterio='Entropía', min_muestras_div=2, max_profundidad=None):
+        """
+        Definición del algoritmo de aprendizaje automático "Árbol de Decisión". La idea es construir un árbol donde 
+        los nodos son preguntas o reglas sobre alguna característica del conjunto de datos (DS), dichas reglas,
+        dividirán al DS en subconjuntos más pequeños según las preguntas o reglas que mejor dividan al DS.
+        
+        Nuestro árbol funciona escogiendo las características que generen los subconjuntos con menor impureza respecto a
+        las etiquetas a predict, utilizando criterios como "gini" o "entropy" si los datos son multiclase o binarios
+        y MSE si los datos son contínuos y requieren de técnicas de regresión.
+        
+        El árbol recibirá una lista de características que dividirá en los nombres de dichas características y sus
+        valores, además de las etiquetas correctas a predict en estructuras de numpy.
+        
+        El árbol recibe como parámetro escrito si el criterio de impureza a utilizar es "gini" o "entropy" además de 
+        hiperparámetros de regularización que el usuario definirá para controlar el modelo, como el máximo de niveles
+        de profundidad del árbol y el mínimo de muestras que debe haber para hacer una nueva división de características.
+        
+        Se inicializa un nodo raíz que, en el proceso de fit al modelo, definirá su regla de división y nodos hijo.
+        """
         # Inicializa el árbol de decisión con los parámetros dados.
         self.caracteristicas = np.array(caracteristicas)
         self.etiquetas = np.array(etiquetas)
@@ -27,13 +55,22 @@ class ArbolDecision:
         self.max_profundidad = max_profundidad
         self.raiz = None
 
-    def entrenar(self):
-        # Entrena el árbol de decisión utilizando los datos proporcionados.
+    def fit(self):
+        """
+        Entrena el árbol de decisión utilizando los datos proporcionados. Llama al proceso de construir un árbol
+        con las características y etiquetas.
+        """
         self.raiz = self._construir_arbol(self.caracteristicas, self.etiquetas, 0)
 
     def _construir_arbol(self, caracteristicas, etiquetas, profundidad_actual):
-        # Construye recursivamente el árbol de decisión.
-        if self._deber_detener_division(etiquetas, caracteristicas.shape[0], profundidad_actual):
+        """
+        Valida si se debe seguir dividiendo el conjunto de datos, en caso afirmativo, busca la mejor regla de
+        división y divide el conjunto de datos en izquierda y derecha según la regla de división y llama 
+        recursivamente a si mismo para construir el árbol de los nodos izquierda y derecha, teniendo cada uno
+        de ellos un nuevo subconjunto de datos. En caso negativo, define el nodo como hoja y representará a una 
+        etiqueta (la etiqueta más común que posea).
+        """
+        if self._detener_division(etiquetas, caracteristicas.shape[0], profundidad_actual):
             return Nodo(es_hoja=True, etiqueta=self._etiqueta_mas_comun(etiquetas))
 
         mejor_regla, mejor_impureza = self._elegir_mejor_regla(caracteristicas, etiquetas)
@@ -51,8 +88,11 @@ class ArbolDecision:
         nodo.muestras = caracteristicas.size
         return nodo
 
-    def _deber_detener_division(self, etiquetas, num_muestras, profundidad_actual):
-        # Determina si se debe detener la división del árbol en un nodo dado.
+    def _detener_division(self, etiquetas, num_muestras, profundidad_actual):
+        """
+        Indica si hay alguna razón para detener el split, ya sea debido a hiperparámetros o debido a que el
+        conjunto ya es totalmente puro.
+        """
         if len(np.unique(etiquetas)) == 1 or num_muestras < self.min_muestras_div:
             return True
         if self.max_profundidad is not None and profundidad_actual >= self.max_profundidad:
@@ -60,11 +100,24 @@ class ArbolDecision:
         return False
 
     def _etiqueta_mas_comun(self, etiquetas):
-        # Devuelve la etiqueta más común en un conjunto de etiquetas.
+        """
+        Devuelve la etiqueta más común en un conjunto de etiquetas.
+        """
         valores, conteos = np.unique(etiquetas, return_counts=True)
         return valores[np.argmax(conteos)]
 
     def _elegir_mejor_regla(self, caracteristicas, etiquetas):
+        """
+        Encuentra la regla que genera la menor impureza respecto a las etiquetas a predict, usa el criterio
+        escogido por el usuario (de momento el criterio es solo "gini" o "entropy") De momento no toma en cuenta
+        el feature si este es continuo.
+        
+        Se evalúa cada valor único de la característica y se busca el que produzca la menor impureza respecto a 
+        las etiquetas, tomando en cuenta si la característica presenta ese valor o no. Se utiliza un promedio ponderado
+        con el cálculo de la impureza, eso consiste en calcular las probabilidades de que la característica presente ese valor único y 
+        de que no lo presente, y multiplicar por la impureza que utiliza como probabilidades las de cada etiqueta respecto
+        a si se presentan cuando el valor del atributo es el evaluado o no, respectivamente.
+        """
         # Selecciona la mejor regla de división para un conjunto de características y etiquetas.
         mejor_impureza = float('inf')
         mejor_regla = None
@@ -99,54 +152,72 @@ class ArbolDecision:
         return mejor_regla, mejor_impureza
 
     def _dividir(self, caracteristicas, regla):
-        # Divide el conjunto de características según una regla dada.
+        """
+        Divide el conjunto de datos dependiendo si cumplen la regla o no.
+        """
         indice_columna, condicion, valor = regla
         indices_izquierda = np.where(caracteristicas[:, indice_columna] == valor)[0]
         indices_derecha = np.where(caracteristicas[:, indice_columna] != valor)[0]
         return indices_izquierda, indices_derecha
 
     def _calcular_impureza(self, etiquetas):
-        # Calcula la impureza según el criterio especificado (entropía o índice de Gini).
+        """
+        Escoge que criterio usar y devuelve la impureza calculada respecto a las etiquetas
+        dependiendo del criterio escogido por el usuario en la definición del árbol de decisión.
+        """
         if self.criterio == 'Entropía':
             return self._calcular_entropia(etiquetas)
         else:
             return self._calcular_gini(etiquetas)
 
     def _calcular_entropia(self, etiquetas):
-        # Calcula la entropía de un conjunto de etiquetas.
+        """
+        Devuelve la impureza utilizando las probabilidades de cada etiqueta usando el criterio entropía.
+        """
         _, conteos = np.unique(etiquetas, return_counts=True)
         probabilidades = conteos / conteos.sum()
         entropia = -np.sum(probabilidades * np.log2(probabilidades))
         return entropia
 
     def _calcular_gini(self, etiquetas):
-        # Calcula el índice de Gini de un conjunto de etiquetas.
+        """
+        Devuelve la impureza utilizando las probabilidades de cada etiqueta usando el criterio gini.
+        """
         _, conteos = np.unique(etiquetas, return_counts=True)
         probabilidades = conteos / conteos.sum()
         gini = 1 - np.sum(probabilidades ** 2)
         return gini
 
-    def predecir(self, caracteristicas):
-        # Predice las etiquetas para un conjunto de características dado.
+    def predict(self, caracteristicas):
+        """
+        Devuelve las predicciones de cada instancia del Dataset.
+        """
         caracteristicas = np.array(caracteristicas)
-        return [self._predecir_individual(caracteristica, self.raiz) for caracteristica in caracteristicas]
+        return [self._predict_individual(caracteristica, self.raiz) for caracteristica in caracteristicas]
 
-    def _predecir_individual(self, caracteristica, nodo):
-        # Predice la etiqueta para una única instancia de características.
+    def _predict_individual(self, caracteristica, nodo):
+        """
+        Determina la predicción para una instancia del dataset dependiendo si sus características cumplen
+        las reglas de los nodos del árbol.
+        """
         if nodo.es_hoja:
             return nodo.etiqueta
         if self._seguir_regla(caracteristica, nodo.regla):
-            return self._predecir_individual(caracteristica, nodo.izquierda)
+            return self._predict_individual(caracteristica, nodo.izquierda)
         else:
-            return self._predecir_individual(caracteristica, nodo.derecha)
+            return self._predict_individual(caracteristica, nodo.derecha)
 
     def _seguir_regla(self, caracteristica, regla):
-        # Evalúa si una instancia de características cumple con una regla dada.
+        """
+        Devuelve el booleano que indica si cumple o no la regla dependiendo si la regla es == o !=.
+        """
         indice_columna, condicion, valor = regla
         return caracteristica[indice_columna] == valor if condicion == '==' else caracteristica[indice_columna] != valor
 
     def imprimir_arbol(self, nodo=None, profundidad=0, condicion="Raíz"):
-        # Imprime la estructura del árbol de decisión.
+        """
+        Imprime el árbol mediante prints.
+        """
         if nodo is None:
             nodo = self.raiz
 
@@ -159,8 +230,12 @@ class ArbolDecision:
             self.imprimir_arbol(nodo.izquierda, profundidad + 1, f"{condicion_str}")
             self.imprimir_arbol(nodo.derecha, profundidad + 1, f"No {condicion_str}")
 
-    def obtener_estructura_arbol(self, nodo=None):
-        # Devuelve la estructura del árbol de decisión en formato JSON.
+    def get_tree_structure(self, nodo=None):
+        """
+        Usa recursión para devolver la estructura completa de un árbol, incluyendo en cada
+        nodo información relevante dependiendo si es un nodo hoja o un nodo de decisión que
+        representa una regla/pregunta.
+        """
         if nodo is None:
             nodo = self.raiz
 
@@ -179,6 +254,6 @@ class ArbolDecision:
             return {
                 "tipo": "Decision",
                 "regla": info,
-                "izquierda": self.obtener_estructura_arbol(nodo.izquierda),
-                "derecha": self.obtener_estructura_arbol(nodo.derecha)
+                "izquierda": self.get_tree_structure(nodo.izquierda),
+                "derecha": self.get_tree_structure(nodo.derecha)
             }
--- view/TreeVisualizer.py
+++ view/TreeVisualizer.py
@@ -1,12 +1,16 @@
 from graphviz import Digraph
 
-class VisualizadorArbol:
+class TreeVisualizer:
     def __init__(self):
-        # Inicializa un objeto Digraph de Graphviz para visualizar el árbol de decisión
+        """
+        Inicializa un objeto Digraph de Graphviz para visualizar el árbol de decisión
+        """
         self.grafo = Digraph(comment='Árbol de Decisión')
 
-    def graficar_arbol(self, estructura_arbol, padre=None, etiqueta_arista=''):
-        # Método recursivo para graficar el árbol de decisión a partir de su estructura
+    def graph_tree(self, estructura_arbol, padre=None, etiqueta_arista=''):
+        """
+        Método recursivo para graficar el árbol de decisión a partir de su estructura
+        """
         if estructura_arbol['tipo'] == 'Hoja':
             # Si es un nodo hoja, establece la etiqueta del nodo como "Hoja: <etiqueta>"
             etiqueta_nodo = f"Hoja: {estructura_arbol['etiqueta']}"
@@ -17,24 +21,27 @@ class VisualizadorArbol:
             nombre_nodo = f"decision_{id(estructura_arbol)}"  # Nombre único para el nodo de decisión
 
             # Recursivamente grafica los subárboles izquierdo y derecho
-            self.graficar_arbol(estructura_arbol['izquierda'], padre=nombre_nodo, etiqueta_arista='Verdadero')
-            self.graficar_arbol(estructura_arbol['derecha'], padre=nombre_nodo, etiqueta_arista='Falso')
+            self.graph_tree(estructura_arbol['izquierda'], padre=nombre_nodo, etiqueta_arista='Verdadero')
+            self.graph_tree(estructura_arbol['derecha'], padre=nombre_nodo, etiqueta_arista='Falso')
 
         if padre:
             # Agrega el nodo actual al grafo y una conexión desde el nodo padre
-            self.grafo.node(nombre_nodo, label=etiqueta_nodo)
+            self.grafo.nodo(nombre_nodo, label=etiqueta_nodo)
             self.grafo.edge(padre, nombre_nodo, label=etiqueta_arista)
         else:
-            # Si es la raíz del árbol, establece la forma del nodo como 'box'
-            self.grafo.node(nombre_nodo, label=etiqueta_nodo, shape='box')
+            # Si es la raíz del árbol, establece la forma del nodo como 'box' y establece como texto el nombre de la etiqueta predicha (target).
+            self.grafo.nodo(nombre_nodo, label=etiqueta_nodo, shape='box')
 
-    def renderizar(self, nombre_archivo='arbol', ver=True):
-        # Renderiza el grafo como un archivo de imagen (por defecto, formato PNG)
+    def get_graph(self, nombre_archivo='arbol', ver=True):
+        """
+        Renderiza el grafo como un archivo de imagen (por defecto, formato PNG)
+        """
         self.grafo.render(nombre_archivo, view=ver, format='png')
 
-
-# Explicación de uso:
-# TreeVisualizer es una clase diseñada para visualizar árboles de decisión utilizando Graphviz.
-# - El método __init__ inicializa un objeto Digraph de Graphviz con un comentario especificado.
-# - El método graph_tree toma la estructura del árbol y genera un grafo que representa el árbol de manera recursiva.
-# - El método render guarda el grafo como un archivo de imagen y lo muestra si se especifica 'view=True'.
+"""
+Explicación de uso:
+TreeVisualizer es una clase diseñada para visualizar árboles de decisión utilizando Graphviz.
+El método __init__ inicializa un objeto Digraph de Graphviz con un comentario especificado.
+El método graph_tree toma la estructura del árbol y genera un grafo que representa el árbol de manera recursiva.
+El método get_graph guarda el grafo como un archivo de imagen y lo muestra si se especifica 'view=True'.
+"""
